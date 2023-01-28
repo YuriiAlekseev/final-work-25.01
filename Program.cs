ee746b9f4d15a9df8da87b43ce49278328b87098
YuriiAlekseev <ash030788@gmail.com>
2023-01-28 12:32:00 +0300

    Окончательные изминения

--- Program.cs
+++ Program.cs
@@ -1,21 +1,27 @@
 ﻿void PrintArray(string [] array)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
     {
-    Console.Write(" " + array[i]); 
+    Console.Write(array[i] +","); 
     }
+Console.Write("]");
 Console.WriteLine();  
 }
 
 string [] FillArray(string [] massiv)
 {
-    for (int i = 0; i < 3; i++)
+    // int len = massiv.Length;
+    for (int i = 0; i <= 3; i++)
     {
         if (massiv[i].Length > 3)
-        {
-            massiv[i] = "";
+        {   
+            Array.Clear(massiv, i, 1);
+            // len = len-1;
         }
     }
+    // Array.Resize(ref massiv, len);
+    // Console.WriteLine(len);
     return massiv;
 }
 
@@ -34,15 +40,19 @@ string [] FillArray(string [] massiv)
 return massiv;
 }
 
+Console.Clear();
 Console.WriteLine("Введите ПЕРВЫЙ массив строк");
 string [] massiv = InputMassiv();
-Console.WriteLine("Введите  ВТОРОЙ строк");
+Console.Clear();
+Console.WriteLine("Введите  ВТОРОЙ массив строк");
 string [] massiv2 = InputMassiv();
+Console.Clear();
 Console.WriteLine("Введите ТРЕТИЙ массив строк");
 string [] massiv3 = InputMassiv();
 string [] array = FillArray(massiv);
 string [] array2 = FillArray(massiv2);
 string [] array3 = FillArray(massiv3);
+Console.Clear();
 PrintArray(array);
 PrintArray(array2);
 PrintArray(array3);
